2ca88bc1165aecf083bb0c0afd283ca53046bd26
@Draichi <lucasdraichi@gmail.com>
2023-03-04 21:00:09 -0300

    chore: run via debug panel

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ This repo is just a backup of some code trying to recreate the Vue engine
 ## Setup
 
 ```sh
-yarn && yarn dev
+yarn
 ```
+
+## Running
+
+On VSCode click on `Run and Debug` then click on `Start debugging`
+
+Update the `price` or `quantity` variables on the Debug Console and see the `total` value update
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+let price = 1;
+let quantity = 3;
+let total = 0;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const dep = new Set();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const effect = () => {
+  total = price * quantity;
+};
+
+function track() {
+  dep.add(effect);
+}
+
+const trigger = () => {
+  dep.forEach((eff: any) => eff());
+};
+
+track();
+effect();
